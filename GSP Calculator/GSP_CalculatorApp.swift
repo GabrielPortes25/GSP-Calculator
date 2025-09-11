e607db1aa037cbf1c16489fd33264a5884d5a97b
GabrielPortes25 <gabrielportescac@gmail.com>
2025-09-11 00:25:11 -0300

    Initial Commit

---
+++ GSP Calculator/GSP_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GSP_CalculatorApp.swift
+//  GSP Calculator
+//
+//  Created by gabriel subutzki portes on 11/09/25.
+//
+
+import SwiftUI
+
+@main
+struct GSP_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
